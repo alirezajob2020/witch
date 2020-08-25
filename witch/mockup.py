dc7d63d415eb0a331dd325a4f3d78b709b7335ab
alirezajob2020 <alirezajob2020@gmail.com>
2020-08-25 10:34:13 +0430

    revising mockup

--- witch/mockup.py
+++ witch/mockup.py
@@ -5,225 +5,205 @@ from witch.models.user import User
 
 def insert():
 
-    user = User(
+    user1 = User(
         title="user1",
         first_name="u1",
         last_name="m1",
         birth_date="1999-5-5",
-        email="user1",
-        password="qweacsd"
+        email="user1@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user1)
 
-    user = User(
+    user2 = User(
         title="user2",
         first_name="u2",
         last_name="m2",
         birth_date="1999-5-5",
-        email="user2",
-        password="qweasd"
+        email="user2@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user2)
 
-    user = User(
+    user3 = User(
         title="user3",
         first_name="u3",
         last_name="m3",
         birth_date="1999-5-5",
-        email="user3",
-        password="qweassd"
+        email="user3@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user3)
 
-    user = User(
+    user4 = User(
         title="user4",
         first_name="u4",
         last_name="m4",
         birth_date="1999-5-5",
-        email="user4",
-        password="qweasd"
+        email="user4@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user4)
 
-    user = User(
+    user5 = User(
         title="user5",
         first_name="u5",
         last_name="m5",
         birth_date="1999-5-5",
-        email="user5",
-        password="qweasd"
+        email="user5@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user5)
 
-    user = User(
+    user6 = User(
         title="user6",
         first_name="u6",
         last_name="m6",
         birth_date="1999-5-5",
-        email="user6",
-        password="qwea2sd"
+        email="user6@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user6)
 
-    user = User(
+    user7 = User(
         title="user7",
         first_name="u7",
         last_name="m7",
         birth_date="1999-5-5",
-        email="user7",
-        password="qweas1d"
+        email="user7@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user7)
 
-    user = User(
+    user8 = User(
         title="user8",
         first_name="u8",
         last_name="m8",
         birth_date="1999-5-5",
-        email="user8",
-        password="qwesd"
+        email="user8@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user8)
 
-    user = User(
+    user9 = User(
         title="user9",
         first_name="u9",
         last_name="m9",
         birth_date="1999-5-5",
-        email="user9",
-        password="qeasd"
+        email="user9@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user9)
 
-    user = User(
+    user10 = User(
         title="user10",
         first_name="u10",
         last_name="m10",
         birth_date="1999-5-5",
-        email="user10",
-        password="qweasd"
+        email="user10@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user10)
 
-    user = User(
+    user11 = User(
         title="user11",
         first_name="u11",
         last_name="m11",
         birth_date="1999-5-5",
-        email="user11",
-        password="qweasd"
+        email="user11@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user11)
 
-    user = User(
+    user12 = User(
         title="user12",
         first_name="u12",
         last_name="m12",
         birth_date="1999-5-5",
-        email="user12",
-        password="qweasd"
+        email="user12@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user12)
 
-    user = User(
+    user13 = User(
         title="user13",
         first_name="u13",
         last_name="m13",
         birth_date="1999-5-5",
-        email="user13",
-        password="qweasd"
+        email="user13@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user13)
 
-    user = User(
+    user14 = User(
         title="user14",
         first_name="u14",
         last_name="m14",
         birth_date="1999-5-5",
-        email="user14",
-        password="qweasd"
+        email="user14@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user14)
 
-    user = User(
+    user15 = User(
         title="user15",
         first_name="u15",
         last_name="m15",
         birth_date="1999-5-5",
-        email="user15",
-        password="qweasd"
+        email="user15@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user15)
 
-    user = User(
+    user16 = User(
         title="user16",
         first_name="u16",
         last_name="m16",
         birth_date="1999-5-5",
-        email="user16",
-        password="qweasd"
+        email="user16@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user16)
 
-    user = User(
+    user17 = User(
         title="user17",
         first_name="u17",
         last_name="m17",
         birth_date="1999-5-5",
-        email="user17",
-        password="qweasd"
+        email="user17@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user17)
 
-    user = User(
+    user18 = User(
         title="user18",
         first_name="u18",
         last_name="m18",
         birth_date="1999-5-5",
-        email="user18",
-        password="qweasd"
+        email="user18@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user18)
 
-    user = User(
+    user19 = User(
         title="user19",
         first_name="u19",
         last_name="m19",
         birth_date="1999-5-5",
-        email="user19",
-        password="qweasd"
+        email="user19@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user19)
 
-    user = User(
+    user20 = User(
         title="user20",
         first_name="u20",
         last_name="m20",
         birth_date="1999-5-5",
-        email="user20",
-        password="qweasd"
+        email="user20@gmail.com",
+        password="Abc123Rez"
     )
-    DBSession.add(user)
-    DBSession.flush()
+    DBSession.add(user20)
 
     DBSession.commit()
 
