edc49d84d14fc9d58509c917578d470fcbd56dc2
alirezajob2020 <alirezajob2020@gmail.com>
2020-08-25 11:47:08 +0430

    adding test_cli_basedata

---
+++ tests/test_cli_basedata.py
@@ -0,0 +1,13 @@
+from witch.models.user import User
+from tests.helpers import LocalApplicationTestCase
+
+
+class TestDatabaseCLI(LocalApplicationTestCase):
+
+    def test_basedata(self):
+        self.__application__.insert_basedata()
+
+        session = self.create_session()
+
+        assert session.query(User).filter(User.title == 'GOD').one()
+        assert session.query(User).count() == 10
