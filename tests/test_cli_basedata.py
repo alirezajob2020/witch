fa08bed77ba335c1c11c64cbb25098d69fe69f43
alirezajob2020 <alirezajob2020@gmail.com>
2020-08-25 11:47:08 +0430

    adding test_cli_basedata

--- tests/test_cli_basedata.py
+++ tests/test_cli_basedata.py
@@ -6,8 +6,8 @@ class TestDatabaseCLI(LocalApplicationTestCase):
 
     def test_basedata(self):
         self.__application__.insert_basedata()
+
         session = self.create_session()
 
         assert session.query(User).filter(User.title == 'GOD').one()
         assert session.query(User).count() == 10
-        
