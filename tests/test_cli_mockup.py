c4ede200fe9e06dfb5ccd2a292967cdd7b3e968d
alirezajob2020 <alirezajob2020@gmail.com>
2020-08-25 12:07:57 +0430

    adding test_cli_mockup

---
+++ tests/test_cli_mockup.py
@@ -0,0 +1,12 @@
+from witch.models.user import User
+from tests.helpers import LocalApplicationTestCase
+
+
+class TestDatabaseCLI(LocalApplicationTestCase):
+
+    def test_mockup(self):
+        self.__application__.insert_mockup()
+        session = self.create_session()
+
+        assert session.query(User).count() == 20
+ 
