33cefea5072ef8d337eea584b1d4c31c4758a26f
alirezajob2020 <alirezajob2020@gmail.com>
2020-09-01 15:31:51 +0430

    revising migration file

--- witch/migration/versions/6236ea34c1f4_alter_from_user_to_member.py
+++ witch/migration/versions/6236ea34c1f4_alter_from_user_to_member.py
@@ -1,15 +1,16 @@
-"""alter user table
+"""alter from user to member
 
-Revision ID: 35ecdbff1b14
-Revises: 
-Create Date: 2020-09-01 10:00:54.987623
+Revision ID: 6236ea34c1f4
+Revises:
+Create Date: 2020-09-01 15:30:20.835522
 
 """
 from alembic import op
 import sqlalchemy as sa
 
+
 # revision identifiers, used by Alembic.
-revision = '35ecdbff1b14'
+revision = '6236ea34c1f4'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,4 +24,6 @@ def upgrade():
 def downgrade():
     op.rename_table('member', 'user')
     op.execute('ALTER SEQUENCE member_id_seq RENAME TO user_id_seq')
+
+
 
