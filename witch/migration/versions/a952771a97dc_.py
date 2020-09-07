8d9308d09350a250b6f5c20ce33f9af62f0ecf89
alirezajob2020 <alirezajob2020@gmail.com>
2020-09-07 10:47:03 +0430

    Adding other value to gender Enum, member model property

---
+++ witch/migration/versions/a952771a97dc_.py
@@ -0,0 +1,24 @@
+"""Add new value to member_gender Enum
+
+Revision ID: a952771a97dc
+Revises: 4d559b227394
+Create Date: 2020-09-06 16:53:44.887983
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'a952771a97dc'
+down_revision = '4d559b227394'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute('COMMIT')
+    op.execute('ALTER TYPE member_gender ADD VALUE \'other\'')
+
+
+def downgrade():
+    pass
